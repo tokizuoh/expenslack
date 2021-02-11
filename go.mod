06214c37c29d35b14789c5bdada7c98cc6b175b8
tokizuoh <k_sakai@yumemi.co.jp>
2021-02-11 19:00:43 +0900

    setback

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/tokizuoh/expenslack
 
 go 1.14
 
-require github.com/joho/godotenv v1.3.0 // indirect
+require (
+	cloud.google.com/go v0.76.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	go.opencensus.io v0.22.6 // indirect
+	golang.org/x/oauth2 v0.0.0-20210210192628-66670185b0cd // indirect
+	google.golang.org/api v0.39.0 // indirect
+	google.golang.org/genproto v0.0.0-20210207032614-bba0dbe2a9ea // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,119 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
+type RequestBody struct {
+	Requests
+}
+
+type Requests []struct {
+	UpdateCells
+}
+
+type UpdateCells struct {
+	Start
+	Rows
+	Fields string
+}
+
+type Start struct {
+	SheetID     int
+	RowIndex    int
+	ColumnIndex int
+}
+
+type Rows []struct {
+	Values
+}
+
+type Values []struct {
+	UserEnteredValue
+}
+
+type UserEnteredValue struct {
+	StringValue string
+}
+
 func main() {
+	// .envの値を取得
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("error")
 	}
-	accessToken := os.Getenv("ACCESS_TOKEN")
-	fmt.Println(accessToken)
+	spreadSheetId := os.Getenv("SPREADSHEET_ID")
+
+	// httpクライアント設定
+	client := &http.Client{}
+	client.Timeout = time.Second * 60
+
+	// リクエストBody作成
+	// values := Values{
+	// 	{
+	// 		UserEnteredValue: UserEnteredValue{
+	// 			StringValue: "hoge",
+	// 		},
+	// 	},
+	// }
+	// fmt.Println(values)
+	requestBody := RequestBody{
+		Requests: Requests{
+			{
+				UpdateCells: UpdateCells{
+					Start: Start{
+						SheetID:     0,
+						RowIndex:    0,
+						ColumnIndex: 0,
+					},
+					Rows: Rows{
+						{
+							Values: Values{
+								{
+									UserEnteredValue: UserEnteredValue{
+										StringValue: "value01",
+									},
+								},
+								{
+									UserEnteredValue: UserEnteredValue{
+										StringValue: "value02",
+									},
+								},
+							},
+						},
+					},
+					Fields: "userEnteredValue",
+				},
+			},
+		},
+	}
+
+	// リクエスト作成
+	b, err := json.Marshal(requestBody)
+	if err != nil {
+		fmt.Println("error...")
+	}
+
+	requestUrl := fmt.Sprintf("https://sheets.googleapis.com/v4/spreadsheets/%s:batchUpdate", spreadSheetId)
+	fmt.Println(requestUrl)
+	req, err := http.NewRequest(http.MethodPost, requestUrl, bytes.NewReader(b))
+	if err != nil {
+		fmt.Println("error!")
+	}
+
+	// リクエストを実行
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error!!")
+	}
+	fmt.Println(res)
+	fmt.Println(1)
+
 }
