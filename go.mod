cc2e656a5d06c03d7665d11ec0bfd3a1957cba5e
tokizuoh <k_sakai@yumemi.co.jp>
2021-02-11 19:24:03 +0900

    add: proceccing stdout member information

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/tokizuoh/expenslack
 
 go 1.14
 
-require github.com/joho/godotenv v1.3.0 // indirect
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/slack-go/slack v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("error")
+		fmt.Printf("%s\n", err)
+		return
 	}
-	accessToken := os.Getenv("ACCESS_TOKEN")
-	fmt.Println(accessToken)
+	slackToken := os.Getenv("SLACK_BOT_TOKEN")
+	slackMemberId := os.Getenv("SLACK_MEMBER_ID")
+
+	api := slack.New(slackToken)
+	user, err := api.GetUserInfo(slackMemberId)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 }
